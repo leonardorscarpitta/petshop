92dc522c4cff776bf6a3b250a003e053898a3f07
Leonardo <leonardorscarpitta@gmail.com>
2024-04-30 20:18:18 -0300

    Ajustando a validação do suporte

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Slideshow
 let carrosselImagens = ['./src/assets/banner1.jpg', './src/assets/banner2.jpg', './src/assets/banner3.jpg'];
 let index = 0;
 let changePicture = 3000;
@@ -12,3 +13,21 @@ function carrossel() {
     setTimeout('carrossel()', changePicture);
 }
 carrossel();
+
+
+// Validação de contato
+const contato = {
+    nome: document.getElementById('nome'),
+    numero: document.getElementById('numero'),
+    assunto: document.getElementById('assunto'),
+    email: document.getElementById('email'),
+    enviar: document.getElementById('btnEnviar')
+}
+
+contato.enviar.addEventListener('click', () => {
+    if (contato.nome.value.length == '' || contato.assunto.value.length == '' || contato.numero.value.length == '' || contato.email.value.length == '') {
+        swal('Verifique os campos em branco!');
+    } else {
+        swal('Mensagem enviada com sucesso! Aguarde pela resposta em seu e-mail!')
+    }
+})
